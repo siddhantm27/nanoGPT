29b1fefd3001c89be2240f144a8bf8eab8b609fc
siddhantm27 <smulkikar@gmail.com>
2026-02-09 01:49:39 +0530

    gpt end of text check

--- model.py
+++ model.py
@@ -359,7 +359,10 @@ class GPT(nn.Module):
             probs = F.softmax(logits, dim=-1)
             # sample from the distribution
             idx_next = torch.multinomial(probs, num_samples=1)
+            if idx_next == '<|endoftext|>':
+                break
             # append sampled index to the running sequence and continue
             idx = torch.cat((idx, idx_next), dim=1)
+
 
         return idx
